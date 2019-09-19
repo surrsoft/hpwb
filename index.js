b7ff236712fd5356c1502b283fc77c9dc7967314
ugin <ugin@cognitivebot.io>
2019-09-19 15:51:16 +0300

    from 06_react

---
+++ index.js
@@ -0,0 +1,21 @@
+const TArray = require('./TArray');
+const TFile = require('./TFile');
+const TInfo = require('./TInfo');
+const TMap = require('./TMap');
+const TNumber = require('./TNumber');
+const TObject = require('./TObject');
+const TString = require('./TString');
+const TUtil = require('./TUtil');
+const TypeID = require('./TypeID');
+
+module.exports = {
+  TArray,
+  TFile,
+  TInfo,
+  TMap,
+  TNumber,
+  TObject,
+  TString,
+  TUtil,
+  TypeID,
+};
