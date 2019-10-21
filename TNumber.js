04d37a3e4b90ce2cc8f4548e623389394e1d6be1
ugin <evgeniy.barannikov@brymonsoft.com>
2019-10-21 15:36:22 +0300

    191021-1536 from node_trvo

--- TNumber.js
+++ TNumber.js
@@ -12,41 +12,44 @@ const TUtil = require('./TUtil');
 
 module.exports = {
   /**
-   * Генерирует случайное целое число в диапазоне 0..(1), включая 0 и (1)
+   * Generate random number in range 0..(1), inclusive 0 and (1)
    *
-   * @param _i {Number} (1) -- целое число, например 2
-   * @return {Number} например: 0, 2, 1, 0, 1, ...
+   * @param _i {Number} (1) -- int number, example 2
+   * @return {Number} example: 0, 2, 1, 0, 1, ...
    */
   random: function (_i) {
     return lodash.random(_i);
   },
 
   /**
-   * Возвращает случайное числов диапазоне между (1) и (2) включая сами эти (1) и (2).
-   * Допускается чтобы (2) был меньше чем (1).
-   * Допускаются отрицательные числа в (1) и (2).
-   * Не похожие на число значения (1)(2) преобразуются в 0.
+   * Return random number in range (1) and (2) inclusively.
+   * Allowed to have (2) less than (1).
+   * Allowed negative number for (1) and (2).
+   * Not that looks as number values (1)(2) converts to 0.
    *
-   * #exception если число переданных параметров != 2
+   * #exception if number arguments != 2
    *
-   * @param _iStart {Number} (1) -- например 2
-   * @param _iEnd {Number} (2) -- например 4
-   * @return {number} например 2 или 3 или 4
+   * @param _iStart {Number} (1) -- example 2
+   * @param _iEnd {Number} (2) -- example 4
+   * @return {number} example 2 or 3 or 4
    */
   random_B: function (_iStart, _iEnd) {
     TUtil.argsCountVerifEx(arguments, 2);
     return lodash.random(_iStart, _iEnd);
   },
 
-  // /** {in work}
-  //  * Генерирует (3) случайных числе из диапазона чисел (1)(2) (включая (1) и (2)).
-  //  * Если (3) больше чем длина диапазона (1)(2) то генерируется ...
-  //  *
-  //  * @param _iStart
-  //  * @param _iEnd
-  //  * @param _iCount
-  //  */
-  // random_C: function (_iStart, _iEnd, _iCount) {
-  //
-  // }
+  /**
+   *
+   * @param _stNumber {String}
+   * @returns {number}
+   */
+  fromString: function (_stNumber) {
+    TUtil.argsCountVerifEx(arguments, 1);
+    if (typeof _stNumber === 'string' && _stNumber.indexOf('.') !== -1) {
+      // drop float
+      return 0;
+    }
+    return lodash.toInteger(_stNumber);
+  }
+
 };
---
+++ test/TNumber.spec.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/* jshint esversion: 8 */
+
+const { expect, assert } = require('chai');
+const { describe, it } = require('mocha');
+const lodash = require('lodash');
+const xrsu = require('../');
+
+
+describe('fromString', () => {
+  it('', () => expect(xrsu.TNumber.fromString('1')).to.equal(1));
+  it('', () => expect(xrsu.TNumber.fromString('f')).to.equal(0));
+  it('', () => expect(xrsu.TNumber.fromString('0')).to.equal(0));
+  it('', () => expect(xrsu.TNumber.fromString('-1')).to.equal(-1));
+  it('', () => expect(xrsu.TNumber.fromString('+1')).to.equal(1));
+  it('', () => expect(xrsu.TNumber.fromString('-0')).to.equal(0));
+  it('', () => expect(xrsu.TNumber.fromString('+0')).to.equal(0));
+  it('', () => expect(xrsu.TNumber.fromString(' ')).to.equal(0));
+  it('', () => expect(xrsu.TNumber.fromString(undefined)).to.equal(0));
+  it('', () => expect(xrsu.TNumber.fromString(null)).to.equal(0));
+  it('', () => expect(xrsu.TNumber.fromString(NaN)).to.equal(0));
+  it('', () => expect(xrsu.TNumber.fromString('1.6')).to.equal(0));
+  it('', () => expect(xrsu.TNumber.fromString('1e5')).to.equal(100000));
+  it('', () => expect(xrsu.TNumber.fromString('1E5')).to.equal(100000));
+  it('', () => expect(xrsu.TNumber.fromString('1E+5')).to.equal(100000));
+  it('', () => expect(xrsu.TNumber.fromString('5a4aa')).to.equal(0));
+  it('', () => expect(xrsu.TNumber.fromString('0010')).to.equal(10));
+});
+
