d48a4e1bd576d098dbf244408c00fbc8593dbed8
ugin <ugin@cognitivebot.io>
2019-11-01 06:48:47 +0300

    191101-0648

--- TMongoDb.js
+++ TMongoDb.js
@@ -9,8 +9,9 @@ CONCEPTS:
 
  */
 
-const { MongoClient, ObjectId } = require('mongodb');
-
+const mongodb = require('mongodb');
+const { MongoClient, ObjectId } = mongodb;
+const util = require('util');
 
 module.exports = {
   /**
@@ -43,23 +44,66 @@ module.exports = {
   },
 
   /**
+   * Create MongoClient
+   *
+   * FOR REFERENCE: getting instance db maked by
+   * <code> db = result-current-function.db(dbName) </code>
+   *
+   * @param _stMongoUrl {String} (1) -- example 'mongodb+srv://surradmin:${password}@clustersurr
+   *   -ovoi0.azure.mongodb.net/${dbName}?retryWrites=true&w=majority'
+   * @return {MongoClient}
+   */
+  mongoClientGet_C: function (_stMongoUrl) {
+    return new MongoClient(
+      _stMongoUrl,
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    );
+  },
+
+  /**
+   * Return info about all dbs
+   *
+   * @param _mongoClient {MongoClient} (1) --
+   * @return {Promise<[Object]>} array of object,
+   *   example [{ name: 'sample_airbnb', sizeOnDisk: 57544704, empty: false }, ...]
+   */
+  dbsInfoGet: async function (_mongoClient) {
+    return new Promise((resolve) => {
+      _mongoClient.connect((err) => {
+        if (err) {
+          throw new Error(err.message);
+        }
+        const db = _mongoClient.db('test');
+        const adminDb = db.admin();
+        adminDb.listDatabases((err, dbs) => {
+          resolve(dbs.databases);
+        });
+        _mongoClient.close();
+      });
+    });
+  },
+
+  /**
    * Get db object
    *
    * @param _stMongoUrl {String} (1) -- example 'mongodb://localhost:27017' (can be with '/' on end)
    * @param _stDbName {String} (2) -- db name, example 'test'
    * @return {Promise<Db>}
    */
-  dbGet: async function (_stMongoUrl, _stDbName){
+  dbGet: async function (_stMongoUrl, _stDbName) {
     const mongoClient = await this.mongoClientGet_B(_stMongoUrl);
     return mongoClient.db(_stDbName);
   },
 
-  /**
-   *
-   * @param _db
-   */
-  dbExists: function (_db) {
-
+  dbGetB: async function (_mongoClient, _stDbName) {
+    return new Promise((resolve) => {
+      _mongoClient.connect((err) => {
+        if (err) {
+          throw new Error(err.message);
+        }
+        resolve(_mongoClient.db(_stDbName));
+        _mongoClient.close();
+      });
+    });
   }
-
 };
