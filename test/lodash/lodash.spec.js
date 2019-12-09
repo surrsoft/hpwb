695aa3b97967c690508d3547a5d6fab749eb759f
ugin <ugin@cognitivebot.io>
2019-12-09 11:01:12 +0300

    191209-1100 mocha run mult

---
+++ test/lodash/lodash.spec.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/* jshint esversion: 8 */
+
+/*
+START: 'npm run test'
+ */
+
+const { expect } = require('chai');
+const { describe, it } = require('mocha');
+const _ = require('lodash');
+
+// describe('TArray.subArray_C', () => {
+//   it('010', () => {
+//     const arr0 = TArray.subArray_C(arr, 2, 2);
+//     expect(arr0).eql([3, 4]);
+//   });
+// });
+
