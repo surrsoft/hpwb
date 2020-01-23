b444e801f39397298e1b1ad5a8864a5ff7ab8f41
ugin <evgeniy.barannikov@brymonsoft.com>
2020-01-23 17:16:43 +0300

    200123-1716

--- TDate.js
+++ TDate.js
@@ -13,7 +13,23 @@ module.exports = {
     const result = new Date(date);
     result.setDate(result.getDate() + days);
     return result;
-  }
+  },
+
+  /**
+   * Return difference date (1) and (2) in milliseconds
+   *
+   * Return 0 if (1) or (2) is not type Date
+   *
+   * @param dtStart {Date}
+   * @param dtEnd {Date}
+   * @return {number}
+   */
+  duration(dtStart, dtEnd) {
+    if (dtStart instanceof Date && dtEnd instanceof Date) {
+      return dtEnd - dtStart;
+    }
+    return 0;
+  },
 
 };
 
